ddb83c327d2391f9c7438c18b4a3514e22bde7b5
anikamcrae <anika@anikamcrae.com>
2014-12-28 19:24:45 +1100

    Complete BlogServiceImpl

--- src/main/java/tutorial/core/services/impl/BlogServiceImpl.java
+++ src/main/java/tutorial/core/services/impl/BlogServiceImpl.java
@@ -8,13 +8,15 @@ import tutorial.core.models.entities.BlogEntry;
 import tutorial.core.repositories.BlogEntryRepo;
 import tutorial.core.repositories.BlogRepo;
 import tutorial.core.services.BlogService;
+import tutorial.core.services.exceptions.BlogNotFoundException;
 import tutorial.core.services.util.BlogEntryList;
 import tutorial.core.services.util.BlogList;
 
 /**
  * Created by Anika McRae on 27/12/2014.
  */
-@Service @Transactional
+@Service
+@Transactional
 public class BlogServiceImpl implements BlogService {
 
     @Autowired
@@ -23,29 +25,44 @@ public class BlogServiceImpl implements BlogService {
     @Autowired
     private BlogEntryRepo blogEntryRepo;
 
+
+
     @Override
     public Blog findBlog(Long blogId) {
-        return null;
+        return blogRepo.findBlog(blogId);
     }
 
 
 
     @Override
     public BlogList findAllBlogs() {
-        return null;
+        return new BlogList(blogRepo.findAllBlogs());
     }
 
 
 
     @Override
     public BlogEntry createBlogEntry(Long blogId, BlogEntry blogEntry) {
-        return null;
+        Blog blog = blogRepo.findBlog(blogId);
+
+        if (blog == null) {
+            throw new BlogNotFoundException();
+        }
+
+        BlogEntry newBlogEntry = blogEntryRepo.createBlogEntry(blogEntry);
+        newBlogEntry.setBlog(blog);
+
+        return newBlogEntry;
     }
 
 
 
     @Override
     public BlogEntryList findAllBlogEntries(Long blogId) {
-        return null;
+        if (blogRepo.findBlog(blogId) == null) {
+            throw new BlogNotFoundException();
+        }
+
+        return new BlogEntryList(blogId, blogEntryRepo.findByBlogId(blogId));
     }
 }
